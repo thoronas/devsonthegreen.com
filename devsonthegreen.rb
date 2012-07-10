8f07ef8bfea8dd3e0693e3e59df336e93bd3674a
brianp <brian.o.pearce@gmail.com>
2012-07-09 17:17:18 -0700

    Less duplication.

--- devsonthegreen.rb
+++ devsonthegreen.rb
@@ -4,21 +4,11 @@ Dir['./models/**/*.rb'].each {|f| require f }
 
 module DevsOnTheGreen 
   class Application < Sinatra::Base
-    register Sinatra::ConfigFile
-    config_file './config.yml'
-
     configure settings.environment.to_sym do
-      db = YAML.load(File.new(settings.root + '/config.yml'))[settings.environment.to_s]
+      register Sinatra::Reloader if development?
 
-      if development?
-        register Sinatra::Reloader
-        DataMapper.setup(:default, db)
-      elsif production?
-        DataMapper.setup(:default, db)
-      else
-        DataMapper.setup(:default, 'sqlite3::memory')
-      end
-
+      db = YAML.load(File.new(settings.root + '/database.yml'))[settings.environment.to_s]
+      DataMapper.setup(:default, db)
       DataMapper.auto_upgrade!
       DataMapper.finalize
     end
